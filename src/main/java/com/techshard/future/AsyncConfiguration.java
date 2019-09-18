a00985b7fc5d2cd4a08b7e1a40099145d959670b
kiran <kkonly4u@gmail.com>
2019-09-18 10:27:33 -0400

    Modified

--- src/main/java/com/techshard/future/AsyncConfiguration.java
+++ src/main/java/com/techshard/future/AsyncConfiguration.java
@@ -1,13 +1,14 @@
 package com.techshard.future;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
@@ -15,16 +16,12 @@ public class AsyncConfiguration {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AsyncConfiguration.class);
 
+    
     @Bean (name = "taskExecutor")
     public Executor taskExecutor() {
         LOGGER.debug("Creating Async Task Executor");
-        final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(2);
-        executor.setMaxPoolSize(2);
-        executor.setQueueCapacity(25);
-        executor.setThreadNamePrefix("CarThread-");
-        executor.initialize();
-        return executor;
+        ExecutorService service = Executors.newFixedThreadPool(2);
+        return service;
     }
 
 }
--- src/main/java/com/techshard/future/controller/CarController.java
+++ src/main/java/com/techshard/future/controller/CarController.java
@@ -1,20 +1,24 @@
 package com.techshard.future.controller;
 
-import com.techshard.future.dao.entity.Car;
-import com.techshard.future.service.CarService;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.function.Function;
+import com.techshard.future.dao.entity.Car;
+import com.techshard.future.service.CarService;
 
 @RestController
 @RequestMapping("/api/car")
